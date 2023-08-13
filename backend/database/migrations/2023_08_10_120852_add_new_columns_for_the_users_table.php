724e895f02646730aced0cfa54278f647772e0f7
Smagin Vladimir <smagin.vovan09@gmail.com>
2023-08-14 02:21:46 +0600

    Added users migrate

---
+++ backend/database/migrations/2023_08_10_120852_add_new_columns_for_the_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('role')->default(User::ROLE_USER);
+            $table->boolean('active')->default(false);
+            $table->integer('order')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropColumns('users', ['role', 'active']);
+    }
+};
