c04ad70f1c7b8470f87c5a541f542498a900d124
Smagin Vladimir <smagin.vovan09@gmail.com>
2023-08-14 02:22:06 +0600

    Added articles migrate

---
+++ backend/database/migrations/2023_08_10_112136_create_articles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("slug");
+            $table->foreignId("category_id")->constrained()->cascadeOnDelete();
+            $table->string('image');
+            $table->string('content');
+            $table->boolean('active')->default(false);
+            $table->integer('order');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+};
